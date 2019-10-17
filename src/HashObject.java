701a57752161be366ce4f53ace7b703c590e25ed
vmcerda <v_ezlife@yahoo.com>
2019-10-17 04:15:28 -0600

    Added Methods to HashObject class

--- src/HashObject.java
+++ src/HashObject.java
@@ -1,3 +1,17 @@
-public class HashObject{
+public class HashObject<V>{
 
+    public int count = 0;
+    public int probeCount = 0;
+
+    public HashObject(){
+
+    }
+
+    public int duplicate(){
+        return count;
+    }
+
+    public int probeCount(){
+        return this.probeCount;
+    }
 }
