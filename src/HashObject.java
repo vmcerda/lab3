87e9a0a3adb31b7a1fdf1884cad27552db4793e9
vmcerda <v_ezlife@yahoo.com>
2019-10-20 11:59:25 -0600

    Methods need completed HashObject

--- src/HashObject.java
+++ src/HashObject.java
@@ -2,15 +2,23 @@ public class HashObject<V>{
 
     public int count = 0;
     public int probeCount = 0;
+    private int key;
 
-    public HashObject(){
+    public HashObject(V obj, int key){
 
     }
-
+    public boolean equals(){
+        return true;
+    }
+    public String toString(){
+        return null;
+    }
+    public int getKey(){
+        return key;
+    }
     public int duplicate(){
         return count;
     }
-
     public int probeCount(){
         return this.probeCount;
     }
--- src/HashTable.java
+++ src/HashTable.java
@@ -1,3 +1,4 @@
-public class HashTable{
+public class HashTable<V>{
+
 
 }
