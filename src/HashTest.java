7e8a91a0f1a990cfd976730f6a26e2f7097b74a7
vmcerda <v_ezlife@yahoo.com>
2019-10-20 16:42:32 -0600

    Methods need completed HashObject

--- src/HashTest.java
+++ src/HashTest.java
@@ -1,6 +1,22 @@
+
 public class HashTest {
 
     public static void main(String[] args){
+        if(args.length < 2){
+            help();
+            System.exit(0);
+        }else{
+            System.out.println(args[0]);
+        }
 
+
+    }
+
+    private static void help() {
+        System.out.println("---Incorrect arguments ---");
+        System.out.println("Usage: java HashTest <input type> <load factor> [<debug level>]");
+        System.out.println("Input type: 1, 2, 3");
+        System.out.println("Load Factor: 0 < x < 1");
+        System.out.println("Debug level: 0 (primary summary) or 1 (summary with hashtable, # of duplicates/probes into a files");
     }
 }
