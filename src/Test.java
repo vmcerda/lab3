44678bdadb413cd80ae46d563990b2ca6d49f0bb
victorcerda <victorcerda@u.boisestate.edu>
2019-10-21 18:39:06 -0600

    Committing Test.java for use later.

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args){
+        PrimeCheck check = new PrimeCheck(95500,96000);
+        System.out.println("HERE");
+        System.out.println(check.getPrime());
+    }
+}
