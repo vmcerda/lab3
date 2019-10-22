d126afb8d986777f45359226173f212e371fb32d
vmcerda <v_ezlife@yahoo.com>
2019-10-21 21:10:16 -0600

    Completed more work on PrimeCheck.  Still needs worked on.  Producing negative calculation numbers

--- src/PrimeCheck.java
+++ src/PrimeCheck.java
@@ -7,64 +7,48 @@ public class PrimeCheck {
     private boolean check;
 
     public PrimeCheck(int minSize, int maxSize){
-//        int start = minSize;
-//        int holdNum = 0;
+
         check(minSize,maxSize);
-//        while(start != maxSize){
-//            int randNum = rand.nextInt();
-//            String biNum = Integer.toBinaryString(minSize-1);
-//            for(int i = 1; i < biNum.length(); i++){
-//                if (biNum.charAt(i) == '1') {
-//                    if(holdNum == 0){
-//                        holdNum = ((int)(Math.pow(randNum,2)%minSize)*randNum)%minSize;
-//                    }else{
-//                        holdNum = ((((int)Math.pow(holdNum,2))%minSize)*randNum)%minSize;
-//                    }
-//                }else{
-//                    holdNum = (int)Math.pow(holdNum,2)%minSize;
-//                }
-//            }
-//            if(holdNum ==1 ){
-//                this.result = minSize;
-//                this.check = true;
-//                break;
-//            }
-//        }
+
     }
     private void check(int minSize,int maxSize){
         int start = minSize;
         int holdNum = 0;
         int prime1 = 0;
         int prime2 = 0;
-        while(start != maxSize){
-            rand = new Random();
-            int randNum = rand.nextInt(start + 1) + 1;
-            String biNum = Integer.toBinaryString(minSize-1);
+        rand = new Random();
+        /*int randNum = rand.nextInt(start + 1) + 1;*/
+        int randNum = rand.nextInt((maxSize - minSize) + 1) + minSize;
+        while(start <= maxSize){
+            String biNum = Integer.toBinaryString(start-1);
             for(int i = 1; i < biNum.length(); i++){
                 if (biNum.charAt(i) == '1') {
                     if(holdNum == 0){
-                        holdNum = (int)(((Math.pow(randNum,2)%minSize)*randNum)%minSize);
+                        holdNum = (((int)((Math.pow(randNum,2)) % start) * randNum) % start);
                     }else{
-                        holdNum = (((((int)Math.pow(holdNum,2))%minSize)*randNum)%minSize); // not working correctly
+                        holdNum = (((int)((Math.pow(holdNum,2)) % start) * holdNum) % start);
                     }
                 }else{
                     if(holdNum == 0){
-                        holdNum = (int)(Math.pow(randNum,2)%minSize);
+                        holdNum = (int)((Math.pow(randNum,2)) % start);
                     }else {
-                        holdNum = ((int)(Math.pow(holdNum, 2)) % minSize);
+                        holdNum = (int)((Math.pow(holdNum, 2)) % start);
                     }
                 }
             }
             if(holdNum == 1 && prime1 == 0){
-                prime1 = minSize;
+                prime1 = start;
+                start++;
                 continue;
             }
             if(holdNum == 1 && prime1 != 0){
-                prime2 = minSize;
-                if((prime2 -prime1) == 2){
+                prime2 = start;
+                if((prime2 - prime1) == 2){
                     result = prime1;
+                    break;
                 }
             }
+            start++;
         }
     }
 
--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args){
-        PrimeCheck check = new PrimeCheck(95500,96000);
+        PrimeCheck check = new PrimeCheck(95748,96000);
         System.out.println("HERE");
         System.out.println(check.getPrime());
     }
