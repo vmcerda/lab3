05d7e5ec0cbacdf67b81c0aca72759fe9df3dbb9
victorcerda <victorcerda@u.boisestate.edu>
2019-10-28 17:31:23 -0600

    Working on PrimeCheck

--- src/Test.java
+++ src/Test.java
@@ -5,47 +5,27 @@ import java.util.function.BinaryOperator;
 public class Test {
 
     public static void main(String[] args) {
-//        PrimeCheck check = new PrimeCheck(95790, 95791);
-//        System.out.println("HERE");
-//        System.out.println(check.getPrime());
         Random rand = new Random();
-//        for(int i = 0; i < 10; i++){
-//            //System.out.println(rand.nextInt((10-5)+1)+5);
-//            //int num = rand.nextInt((5 - 1) - 1 ) + 2;
-//            int start = 5;
-//            int num = rand.nextInt(start - 2) + 2;
-//            System.out.println(num);
-//        }
         int n = 95791;
-        int a = 0;
-        int b = (int)(Math.log(n) / Math.log(2));
-        int randNum = rand.nextInt(n - 2) + 2;
-        b = b-1; // start on 2nd MSB
-        //msb = (int)(Math.log(n) / Math.log(2));
-        //int a = (int)(Math.pow(2, msb));
-        //System.out.println(a);
-        int holdNum = 0;
+        int b = (int)(Math.log(n) / Math.log(2) + 1) -1;
+        //int randNum = rand.nextInt(n - 2) + 2;
+        int randNum = 13;
+        BigInteger holdNum = new BigInteger(Integer.toString(randNum)); //Holding randNumber so it can change after 1 or 0;
+        BigInteger b1 = new BigInteger(Integer.toString(n)); // Holding table length;
+        BigInteger b2 = new BigInteger(Integer.toString(randNum)); //Holding randNumber to be used consistently in equation
+//        BigInteger holdNum = new BigInteger(("0"));
         while (b >= 0) {
-            a = (n >> (b));
-            //System.out.println(a%2);
+            int a = (n >> (b));
             if(a%2 == 1){
-                holdNum = ((int)(Math.pow(randNum, 2)) % n);
-//                if(holdNum == 0) {
-//                    //holdNum = (((int) ((Math.pow(randNum, 2)) % n) * randNum) % n);
-//                }else{
-//                    //holdNum = (((int) ((Math.pow(holdNum, 2)) % n) * randNum) % n);
-//                }
+                holdNum = holdNum.pow(2).multiply(b2).mod(b1);
+//                holdNum = (((int) ((Math.pow(randNum, 2)) % n) * randNum) % n);
+//                holdNum = (((int) ((Math.pow(holdNum, 2)) % n) * randNum) % n);
             }else{
-                holdNum = ((int)(Math.pow(randNum, 2)) * randNum) % n;
-//                if(holdNum == 0){
-//                    holdNum = (int)((Math.pow(randNum,2)) % n);
-//                }else {
-//                    holdNum = (int) ((Math.pow(holdNum, 2)) % n);
-//                }
+                holdNum = holdNum.pow(2).mod(b1);
+                //holdNum= ((int)(Math.pow(holdNum, 2)) % n);
             }
             b = b -1;
         }
-        System.out.println(randNum);
-        System.out.println(holdNum);
+        System.out.println(holdNum.toString());
     }
 }
